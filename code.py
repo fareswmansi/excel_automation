d521df64606e000bb783f9680f294a5b5ec08b97
fareswmansi <fareswmansi@gmail.com>
2020-06-24 12:23:33 +0300

    eleventh commit

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 from openpyxl import workbook, load_workbook
-import pandas as pd
 import numpy as np
-import xlrd as xl
+from functions import checking_coordinates
 
 
 #lists for data storage and automation processes
@@ -94,22 +93,7 @@ if (first_choice == 'YES' or first_choice == 'yes'):
 
 
                 #error catching, check if coordinates actually exist and match the strings
-
-                def checking_coordinates(matched_strings, coordinates_list):
-                    i = 0
-                    while i < len(coordinates_list):
-                        i += 1
-                        if i < len(coordinates_list):
-                            c = sheet[coordinates_list[i]]
-                            for phone_number in matched_strings:
-                                j = 0
-                                while j < len(matched_strings):
-                                    j += 1
-                                    if j < len(matched_strings):
-
-
-                        else:
-                            break
+                checking_coordinates(matched_strings, coordinates_list)
 
 
 
