bf981f28904d2abaea751b7e163c447d0a143ccc
fareswmansi <fareswmansi@gmail.com>
2020-06-27 11:38:50 +0300

    34th commit

--- functions.py
+++ functions.py
@@ -100,20 +100,22 @@ def match_coordinate_with_input(add_to_these_coordinates, database_list_of_lists
                     area_input_list.append(area_input)
                     location_input_list.append(location_input)
 
-#input area and location into matched_strings in excel
-def input_excel(location_input_list, area_input_list, order_of_input_list, add_to_these_coordinates, final_coordinate_list):
+#input area into matched string index in excel
+def area_input_excel(area_input_list, order_of_input_list, add_to_these_coordinates, final_coordinate_list):
     i = 0
     while i < len(order_of_input_list):
         i += 1
         if i < len(order_of_input_list):
             sheet[add_to_these_coordinates[i]] = area_input_list[i]
-            workbook.save('testme2.xlsx')
             for coordinate in add_to_these_coordinates:
                 final_index = coordinate.replace('M', 'Q')
                 final_coordinate_list.append(final_index)
-                j = 0
-                while j < len(order_of_input_list):
-                    j += 1
-                    if j < len(order_of_input_list):
-                        sheet[final_coordinate_list[j]] = location_input_list[j]
-                        workbook.save('testme2.xlsx')
+
+#input location into matched string index in excel
+def location_input_excel(location_input_list, final_coordinate_list, order_of_input_list):
+    i = 0
+    while i < len(order_of_input_list):
+        i += 1
+        if i < len(order_of_input_list):
+            sheet[final_coordinate_list[i]] = location_input_list[i]
+            workbook.save('testme2.xlsx')
